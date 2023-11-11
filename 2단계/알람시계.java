dd677f3f1d3161bf9247688d5f302d16c5ed490e
HyeonWoo Shin <145894090+bluelight1010@users.noreply.github.com>
2023-11-11 15:37:04 +0900

    Create 알람시계.java

---
+++ 2단계/알람시계.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        String[] time = input.split(" ");
+        int H = Integer.parseInt(time[0]);
+        int M = Integer.parseInt(time[1]);
+        
+        if(M<45){
+            	H--;
+			M = 60 - (45 - M);
+			if(H < 0) {
+				H = 23;
+			}
+			System.out.println(H + " " + M);
+		}
+		else {
+			System.out.println(H + " " + (M - 45));
+		}
+       }
+   }
