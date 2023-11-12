631399095aeff1a35a3d613ee98becdab8717e0e
902N <145334908+902N@users.noreply.github.com>
2023-11-12 17:31:10 +0900

    Create 문자와 문자열.java

---
+++ 4단계/문자와 문자열.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String S = sc.nextLine();
+		int i = sc.nextInt();
+		
+		System.out.println(S.charAt(i-1));
+
+	}
+}
