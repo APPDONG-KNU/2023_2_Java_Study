2a48109b4e619754d680fd8ad9f52288d8470ad7
sjo5368 <139030683+sjo5368@users.noreply.github.com>
2023-11-21 10:48:21 +0900

    Update prob1002.java

--- prob1002.java
+++ prob1002.java
@@ -14,17 +14,24 @@ public class Main {
 			 int x2=sc.nextInt();
 			 int y2=sc.nextInt();
 			 int r2=sc.nextInt();
+//두 점사이의 거리와 두 점을 중심으로 하는 두 원의 반지름을 비교하여 두 원의 만나는 점을 파악
 			 double distance = Math.pow(x2-x1,2)+Math.pow(y2-y1,2);
+//두 원이 일치할 때
 			 if (x1==x2 && y1==y2 && r1==r2)
 				 arr[i]=-1;
+//두 원이 만나지 않을 때
 			 else if (distance>Math.pow(r1+r2,2))
 				 arr[i]=0;
+//한 원이 다른 원의 내부에 있고, 만나지 않을 때
 			 else if (distance<Math.pow(r2-r1,2))
 				 arr[i]=0;
+//두 원이 내접할 때
 			 else if (distance==Math.pow(r2 - r1, 2))
-				 arr[i]=1;			 
+				 arr[i]=1;
+//두 원이 외접할 때			 
 			 else if (distance==Math.pow(r1+r2, 2))
 				 arr[i]=1;
+//두 원이 두점에서 만날 때
 			 else
 				 arr[i]=2;
 		 	}
